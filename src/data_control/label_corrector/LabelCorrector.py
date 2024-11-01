10350cea22815ad224d430c6c945259d67fa704b
rasauq1122 <rasauq1122@gmail.com>
2024-11-01 13:43:11 +0900

    feat: add LabelCorrector (abstract class)

---
+++ src/data_control/label_corrector/LabelCorrector.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+
+class LabelCorrector(ABC):
+    
+    @abstractmethod
+    def correct(self, df: pd.DataFrame) -> pd.DataFrame:
+        """dataframe의 모든 행의 noise가 없다고 가정하고, 
+           모든 행의 label을 교정한다.
+
+        Args:
+            df (pd.DataFrame): noise가 없는 dataframe
+
+        Returns:
+            pd.DataFrame: label이 교정된 dataframe
+        """
+        pass
