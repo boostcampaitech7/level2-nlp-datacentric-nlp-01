349222d9c0e03b10e978354e8c390df5f9a3cfbd
rasauq1122 <rasauq1122@gmail.com>
2024-11-04 15:54:42 +0900

    refactor: modify main.py to add data control module test option
    - Refactor main.py to include an option for running the data control module test.
    - Add argparse to parse command line arguments.
    - Add --data_control flag to enable the data control module test.
    - Call module_test() if the --data_control flag is provided.
    - Otherwise, continue with the usual training and evaluation process.

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from src.train import trainer
 from src.evaluate import evaluate_and_save
+from src.dc_tester import module_test 
+import argparse
 
 if __name__ == '__main__':
-    trainer.train()
-    evaluate_and_save()  # 학습 후 테스트 데이터 예측 및 결과 저장
+    parser = argparse.ArgumentParser()
+    parser.add = parser.add_argument('--data_control', action='store_true', help='Run data control module test')
+    args = parser.parse_args()    
+
+    if args.data_control:
+        module_test()
+    else:
+        trainer.train()
+        evaluate_and_save()  # 학습 후 테스트 데이터 예측 및 결과 저장
