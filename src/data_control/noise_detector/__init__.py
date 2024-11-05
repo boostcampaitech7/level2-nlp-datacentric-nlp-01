176e0f5d9cb1ac4acb7db12e225e6f3289132908
rasauq1122 <rasauq1122@gmail.com>
2024-11-05 14:29:59 +0900

    feat: add non-space ascii ratio detector

--- src/data_control/noise_detector/__init__.py
+++ src/data_control/noise_detector/__init__.py
@@ -1,4 +1,5 @@
 from .NoiseDetector import NoiseDetector
 from .NoiseDetectorASCII import NoiseDetectorASCII
+from .ASCIIwithoutSpace import ASCIIwithoutSpace
 
-__all__ = ['NoiseDetector', 'NoiseDetectorASCII']
+__all__ = ['NoiseDetector', 'NoiseDetectorASCII', 'ASCIIwithoutSpace']
