37bfde99ff35b6a54a9af85a37082f68284e44dc
hskhyl <155405525+hskhyl@users.noreply.github.com>
2024-11-05 19:16:47 +0900

    renewal project constructure

--- src/model.py
+++
@@ -1,6 +0,0 @@
-import torch
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-from src.config import DEVICE, MODEL_NAME
-
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=7).to(DEVICE)
--- src/utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import evaluate
-
-f1 = evaluate.load('f1')
-
-def compute_metrics(eval_pred):
-    predictions, labels = eval_pred
-    predictions = np.argmax(predictions, axis=1)
-    return f1.compute(predictions=predictions, references=labels, average='macro')
